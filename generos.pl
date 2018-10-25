ae84c546d66c940572c57808967d4a66aa966333
elenalimones <44365823+elenalimones@users.noreply.github.com>
2018-10-25 22:04:52 +0200

    Update generos.pl

--- generos.pl
+++ generos.pl
@@ -1,22 +1,24 @@
 #!/usr/bin/perl
 print "Content-Type: text/html\n\n";
-if($ENV{'QUERY_STRING'} eq "") {
-    print "<h1>Elige tu genero favorito: </h1>
-          <form name=\"search\" method=\"get\" >";
-   open F, "/home/elenalimones/generos.txt";
-   while(<F>) {
-     @campos=split(":");
-     $ar{$campos[6]}=$campos[6];
-   }
-   print "<select name = \"opciones\">";
-   for (keys %ar){
-     print "<option value=$_";
-     print ">$_</option>";
-   }
-   print "</select>
-         <input type=\"submit\" value=\"Elegir\" />";
-}else {
-   @arGet=split("=",$ENV{'QUERY_STRING'});
-   $arGet[1]=~ s/%2F/\//g;
-   print "Elegiste $arGet[1]<br>Buen genero, felicidades<br><i></i>";
+
+@pairs = split(/&/, $ENV{QUERY_STRING});
+
+foreach $pair (@pairs) {
+   ($name, $value) = split(/=/, $pair);
+   $value =~ tr/+/ /;
+   $value =~ s/%(..)/pack("C", hex($1))/eg;
+   $FORM{$name} = $value;
+}
+
+$first_name = $FORM{first_name};
+%valores = ( 'comedia','drama','suspense' );
+
+if($first_name == 'comedia' ) {
+  print("Podria aconsejarte peliculas como Resacon en las Vegas o Es por tu bien","\n");
+}
+if($valores{$first_name} == 'drama' ) {
+    print("Podria aconsejarte peliculas como Lo imposible o La vida es bella","\n");
+}
+if($valores{$first_name} == 'suspense' ) {
+    print("Podria aconsejarte peliculas como Perdida o El regalo","\n");
 }
