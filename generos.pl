e4ad191927ace791724d289316aab11af3229723
elenalimones <44365823+elenalimones@users.noreply.github.com>
2018-11-02 14:40:14 +0100

    Update generos.pl

--- generos.pl
+++ generos.pl
@@ -6,30 +6,30 @@ if($ENV{'QUERY_STRING'} eq "") {
           <form method=\"get\" >";
 
 print   "<input type=\"text\"  name=\"nombre\" />";
-print   "<input type=\"submit\" value=\"Search\" />";
+print   "<input type=\"submit\" value=\"Enviar\" />";
 }
 if($ENV{'QUERY_STRING'} ne "") {
    @arGet=split("=",$ENV{'QUERY_STRING'});
-   if (@arGet[0] eq "nombre") {
-   $arGet[1]=~ s/%2F/\//g;
-   print "<h1>Elija su genero de peliculas favorito </h1>
+   if ($arGet[0] eq "nombre") {
+     $arGet[1]=~ s/%2F/\//g;
+     print "<h1>Elija su genero de peliculas favorito </h1>
           <form name=\"search\" method=\"get\" >";
-   open F, "/home/usuario/generos";
-   while(<F>) {
-     @campos=split(":");
-     $ar{$campos[6]}=$campos[6];
-   }
-   print "<select name = \"shell\">";
-   for (keys %ar){
+     open F, "/home/elenalimones/generos";
+     while(<F>) {
+      @campos=split(":");
+      $ar{$campos[1]}=$campos[1];
+
+    }
+    print "<select name = \"genero\">";
+    for (keys %ar){
      print "<option value=$_";
      print ">$_</option>";
+    }
+    print "</select>
+         <input type=\"submit\" value=\"Enviar\" />";
    }
-   print "</select>
-         <input type=\"submit\" value=\"Search\" />";
-}
-}
-
-   if (@arGet[0] eq "shell") {
-   $arGet[1]=~ s/%2F/\//g;
-   print "De acuerdo. Muchas gracias";
+   if ($arGet[0] eq "genero") {
+       $arGet[1]=~ s/%2F/\//g;
+       print "Buena eleccion la del genero de $arGet[1]. Muchas gracias";
+   }
 }
